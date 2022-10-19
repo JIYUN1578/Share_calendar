9b896f47c2218487c608d1884db772ec4fc44be0
thswhdgk <ds10079@naver.com>
2022-10-20 03:27:43 +0900

    [20221020 손종하] 액티비티 5차 수정

--- app/src/main/java/ort/techtown/share_calendar/PostActivity.java
+++ app/src/main/java/ort/techtown/share_calendar/PostActivity.java
@@ -4,19 +4,38 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import ort.techtown.share_calendar.Adapter.CalendarAdapter;
+import ort.techtown.share_calendar.Adapter.TodoListAdapter;
+import ort.techtown.share_calendar.Data.CalendarUtil;
+import ort.techtown.share_calendar.Data.Info;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -30,15 +49,53 @@ public class PostActivity extends AppCompatActivity {
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = database.getReference();
     private DatabaseReference mReference = database.getReference();
+    // 달력
+    TextView tv_monthyear;
+    RecyclerView recyclerView, todoListRecyclerView;
+    // 그룹 정보
+    private String groupname, uid;
+    private TextView group_name, group_introduce;
+    private ImageView group_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        String groupname = getIntent().getStringExtra("groupname");
-        String uid = getIntent().getStringExtra("uid");
+        groupname = getIntent().getStringExtra("groupname");
+        uid = getIntent().getStringExtra("uid");
 
+        // 달력
+        tv_monthyear = findViewById(R.id.tv_month_year);
+        ImageButton btn_pre = findViewById(R.id.btn_frontmonth);
+        ImageButton btn_next = findViewById(R.id.btn_nextmonth);
+        recyclerView = findViewById(R.id.recyclerview);
+        todoListRecyclerView = findViewById(R.id.todoListRecyclerView);
+        CalendarUtil.today = LocalDate.now();
+        CalendarUtil.selectedDate = LocalDate.now();
+        setTodoList(uid);
+        // 화면 설정
+        setMonthview();
+        btn_pre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 한달 전전
+                CalendarUtil.selectedDate = CalendarUtil.selectedDate.minusMonths(1);
+                setMonthview();
+                setTodoList(uid);
+            }
+        });
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 한달 뒤
+                CalendarUtil.selectedDate = CalendarUtil.selectedDate.plusMonths(1);
+                setMonthview();
+                setTodoList(uid);
+            }
+        });
+
+        // 툴바
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
@@ -120,6 +177,122 @@ public class PostActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // 그룹 정보 받아오기
+        group_name = (TextView)findViewById(R.id.group_name);
+        group_introduce = (TextView)findViewById(R.id.group_introduce);
+        group_image = (ImageView)findViewById(R.id.group_image);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("Group").child(groupname).child("groupname").getValue().toString();
+                String introduce = snapshot.child("Group").child(groupname).child("introduce").getValue().toString();
+                group_name.setText(name);
+                group_introduce.setText(introduce);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    // 달력
+    private void setMonthview() {
+        // 년월 텍스트뷰 셋팅
+        tv_monthyear.setText(monthYearFromDate(CalendarUtil.selectedDate));
+        // 해당 월 날짜 가져오기
+        ArrayList<LocalDate> dayList = daysInMonthArray(CalendarUtil.selectedDate);
+        // 어뎁터 데이터 적용
+        CalendarAdapter adapter = new CalendarAdapter(dayList);
+        // 레이아웃 설정 열 7개
+        RecyclerView.LayoutManager manager = new GridLayoutManager(getApplicationContext(),7);
+        // 레이아웃 적용
+        recyclerView.setLayoutManager(manager);
+        // 어뎁터 적용
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new CalendarAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                setTodoList(uid);
+            }
+        });
+    }
+    private void setTodoList(String uid) {
+        // 해당 일정 가져오기
+        ArrayList<Info> infolist = new ArrayList<>();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference;
+
+        TodoListAdapter adapter = new TodoListAdapter(infolist);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getApplicationContext());
+        // 어뎁터 적용
+        todoListRecyclerView.setLayoutManager(manager);
+        todoListRecyclerView.setAdapter(adapter);
+        try{
+            reference = database.getReference("User").child(uid).child("Calender").child(CalendarUtil.selectedDate.toString());
+            Query myTopPostsQuery = reference.orderByChild("start");
+            myTopPostsQuery.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        infolist.add(snapshot.getValue(Info.class));
+                        Log.e("111",infolist.toString());
+                        TodoListAdapter adapter = new TodoListAdapter(infolist);
+                        RecyclerView.LayoutManager manager = new LinearLayoutManager(getApplicationContext());
+                        // 어뎁터 적용
+                        todoListRecyclerView.setLayoutManager(manager);
+                        todoListRecyclerView.setAdapter(adapter);
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "지금 안돼유",Toast.LENGTH_SHORT).show();
+            Log.e("222",infolist.toString());
+        }
+        // 어뎁터 데이터 적용
+    }
+    // 날짜 타입 설정
+    private String monthYearFromDate(LocalDate localDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM월 yyyy");
+        return localDate.format(formatter);
+    }
+
+    private ArrayList<LocalDate>  daysInMonthArray(LocalDate localDate){
+        ArrayList<LocalDate> daylist = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(localDate);
+        // 해당 월 마지막 날짜 가져오기
+        int lastday = yearMonth.lengthOfMonth();
+        // 해당월의 첫번째 날짜 가져오기
+        LocalDate firstday = CalendarUtil.selectedDate.withDayOfMonth(1);
+        // 첫번째 날 요일 가져오기
+        int dayOfweek = firstday.getDayOfWeek().getValue();
+        // 날짜 생성
+        for(int i = 1 ; i <= 42 ; i++){
+            if(i <=dayOfweek){
+                LocalDate premonthDate = CalendarUtil.selectedDate.minusMonths(1);
+                int lastdayOfpremonth = premonthDate.lengthOfMonth();
+                daylist.add(LocalDate.of(premonthDate.getYear(), premonthDate.getMonth(),
+                        lastdayOfpremonth - dayOfweek + i ));
+                Log.d("달력 남은 날짜", premonthDate.toString() );
+            }
+            else if(i> lastday + dayOfweek){
+                LocalDate nextmonthDate = CalendarUtil.selectedDate.plusMonths(1);
+                daylist.add(LocalDate.of(nextmonthDate.getYear(),
+                        nextmonthDate.getMonth(),
+                        i - lastday-dayOfweek));
+                Log.d("달력 남은 날짜",nextmonthDate.toString() );
+            }else{
+                daylist.add(LocalDate.of(CalendarUtil.selectedDate.getYear(),
+                        CalendarUtil.selectedDate.getMonth(),
+                        i - dayOfweek));
+            }
+        }
+        return daylist;
     }
 
     // drawerLayout 리스너
